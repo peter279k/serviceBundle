0c3fbb42f73ee5fe6ed77d889686498d01f5b2ae
Joe Fearnley <joe.fearnley@gmail.com>
2017-10-29 17:51:34 -0400

    Adding tests directory and initial files

---
+++ tests/serviceBundle/MailgunTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use peter\components\serviceBundle\ServiceBundle;
+
+/**
+* 
+*/
+class ClassName extends TestCase
+{
+    
+    function __construct(argument)
+    {
+        # code...
+    }
+}
