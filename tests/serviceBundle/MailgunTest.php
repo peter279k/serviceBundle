c9248f1b3319a751dc823083aebdd79c7a9f1461
Joe Fearnley <joe.fearnley@gmail.com>
2017-11-01 17:43:47 +0000

    creating mailgun tests

--- tests/serviceBundle/MailgunTest.php
+++ tests/serviceBundle/MailgunTest.php
@@ -15,16 +15,11 @@ class MailgunTest extends TestCase
     /** @test */
     public function canSendEmail()
     {
-        $fakeResponse = [
+        $mailgunService = $this->getMockBuilder('peter\components\serviceBundle\Mailgun')->getMock();
+        $mailgunService->method('sendReq')->willReturn([
             'id' => '<20171031193543.106121.C5D2A970EE25C57B@sandboxeff4a9e74ada4d2fadf041733ba287c3.mailgun.org>',
             'message' => 'Queued. Thank you.'
-        ];
-        $mailgun = $this->getMockBuilder('peter\components\serviceBundle\Mailgun')
-                ->disableOriginalConstructor()
-                ->getMock();
-
-        $mailgun->method('sendReq')
-            ->willReturn($fakeResponse);
+        ]);
 
         $configs = [
             'api-key' => 'key-1234567890',
@@ -35,12 +30,9 @@ class MailgunTest extends TestCase
             'contents' => 'Mailgun is awesome !',
         ];
 
-        $mailgunService = (new ServiceFactory)->create('mailgun');
         $mailgunService->setConfigs($configs);
         $response = $mailgunService->sendReq();
 
-        echo '<pre>'; var_dump($response); echo '</pre>'; die();
-
-        $this->assertEquals('Queued. Thank you.', $response['message']);
+        $this->assertSame('Queued. Thank you.', $response['message']);
     }
 }
